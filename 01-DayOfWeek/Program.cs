48a229bf5663b468e39ff185c7809f4027e2a684
denisYordanov-eng <denisivov437@gmail.com>
2025-04-29 09:39:10 +0300

    Just made a task using dateTime;

---
+++ 01-DayOfWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateAsString = Console.ReadLine();
+            DateTime day = DateTime.ParseExact(dateAsString, "d-M-yyyy"
+                , CultureInfo.InvariantCulture);
+            Console.WriteLine(day.DayOfWeek);
+        }
+    }
+}
