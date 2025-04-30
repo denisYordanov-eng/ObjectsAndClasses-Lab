7a344cdde852716df24386ec99eabfd5b11dcd5d
denisYordanov-eng <denisivov437@gmail.com>
2025-04-30 05:42:24 +0300

    made a task using .Random method

---
+++ 02-RandomizeWords/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+
+            Random rndm = new Random();
+
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+            {
+              
+                int pos2 = rndm.Next(0, words.Length);
+
+                string newPositionWords = words[pos2];
+                string oldPositionWords = words[pos1];
+
+                words[pos1] = newPositionWords;
+                words[pos2] = oldPositionWords;
+                
+            }
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
