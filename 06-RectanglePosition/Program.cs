89a2de1c9ef2452c48a69bc7ebadc1f2b3f8f69a
denisYordanov-eng <denisivov437@gmail.com>
2025-05-02 19:16:20 +0300

    Made a task , trying to get comfortable using class objects and method

---
+++ 06-RectanglePosition/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_RectanglePosition
+{
+    class Program
+    {
+        class Rectangle
+        {
+            public int Top { get; set; }
+            public int Left { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            
+
+            public int Bottom
+            {
+                get
+                {
+                    return Top + Height;
+                }
+            }
+            public int Right
+            {
+                get
+                {
+                    return Left + Width;
+                }
+            }
+
+            public bool IsInside(Rectangle other)
+            {
+                var isInLeft = Left >= other.Left;
+                var isInRight = Right <= other.Right;
+                var isInsideHorizontal = isInLeft && isInRight;
+
+                var isInTop = Top >= other.Top;
+                var isInBottom = Bottom <= other.Bottom;
+                var isInsideVertical = isInTop && isInBottom;
+
+                return isInsideHorizontal && isInsideVertical;
+            }
+            static void Main(string[] args)
+            {
+                Rectangle r1 = ReadRectangle(), r2 = ReadRectangle();
+                Console.WriteLine(r1.IsInside(r2) ? "Inside" : "Not inside");
+
+            }
+        }
+         static Rectangle ReadRectangle()
+        {
+            var size = Console.ReadLine().Split(' ').Select(a => int.Parse(a));
+
+            Rectangle rectangle = new Rectangle()
+            {
+                Left = size.First(),
+                Top = size.Skip(1).First(),
+                Width = size.Skip(2).First(),
+                Height = size.Skip(3).First()
+            };
+            return rectangle;
+        }
+    }
+}
