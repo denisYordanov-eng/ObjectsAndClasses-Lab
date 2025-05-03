1814437172613412586b0306c3fce397ed3f5c68
denisYordanov-eng <denisivov437@gmail.com>
2025-05-03 15:15:45 +0300

    Made a task.

---
+++ Songs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Songs
+{
+    class Program
+    {
+        class Song
+        {
+            public string TypeList { get; set; }
+            public string Name { get; set; }
+            public string Time { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            int songsCount = int.Parse(Console.ReadLine());
+
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < songsCount; i++)
+            {
+                string[] input = Console.ReadLine().Split('_');
+
+                string style = input[0];
+                string nameSong = input[1];
+                string songLength = input[2];
+
+                Song song = new Song();
+
+                song.TypeList = style;
+                song.Name = nameSong;
+                song.Time = songLength;
+
+                songs.Add(song);
+            }
+            string typeList = Console.ReadLine();
+
+            if (typeList == "all")
+            {
+                foreach (var song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (var song in songs.Where(a => a.TypeList == typeList))
+                {
+                    if (song.TypeList == typeList)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }  
+    }
+}
