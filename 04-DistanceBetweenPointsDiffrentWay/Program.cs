1c2c9fa8388d630b1374d597ff3af674f2235d3c
denisYordanov-eng <denisivov437@gmail.com>
2025-05-03 11:01:09 +0300

    Made a task in a different way for me

---
+++ 04-DistanceBetweenPointsDiffrentWay/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_DistanceBetweenPointsDiffrentWay
+{
+    class Program
+    {
+        class Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+           p1 =  ReadInput();
+            Point p2 = new Point();
+            p2 = ReadInput();
+
+          var result =  CalculateDistanceBetweenPoint(p1, p2);
+            Console.WriteLine("{0:f3}", result);
+        }
+        static double CalculateDistanceBetweenPoint(Point p1, Point p2)
+        {
+            double diffX = p2.X - p1.X;
+            double diffY = p2.Y - p1.Y;
+            return Math.Sqrt((diffX * diffX) + (diffY * diffY));
+        }
+
+        static Point ReadInput()
+        {
+            double[] input = Console.ReadLine()
+                .Split(' ')
+                .Select(a => double.Parse(a))
+                .ToArray();
+
+            Point point = new Point();
+            point.X = input[0];
+            point.Y = input[1];
+            return point;
+        }
+    }
+}
